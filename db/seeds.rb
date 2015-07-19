8f25ae9da0c3426303d85755df30520a37815094
Yamato <wonka.bm@gmail.com>
2015-07-20 03:24:55 +0900

    re-create users table & add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |i|
+  User.create(name: "test_seed#{i}", participation: "TECH::CAMP #{i}", occupation: "Mentor", profile: "seed test", email: "test+seed#{i}@test.com", password: "12121212")
+end
