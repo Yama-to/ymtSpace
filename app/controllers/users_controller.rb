d173a481c65f052a034798e75327bd4b88474d2c
Yamato <wonka.bm@gmail.com>
2015-07-25 11:43:09 +0900

    create users'page & edit functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,32 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @prototypes = @user.prototypes.page(params[:page]).per(3)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      flash[:danger] = "Access denied."
+      redirect_to root_path
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.id == current_user.id
+      user.update(update_params)
+      flash[:success] = "Successfully updated your information."
+      redirect_to action: :show
+    else
+      flash[:danger] = "Bad access."
+      redirect_to action: :edit
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name, :profile, :participation, :occupation)
   end
 end
