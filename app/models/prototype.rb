22b7f7249d699abc60cac83013830deff1a6b758
Yamato <wonka.bm@gmail.com>
2015-07-25 12:06:48 +0900

    define methods & validations for prototypes

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,10 +1,42 @@
 class Prototype < ActiveRecord::Base
-  has_many :thumbnails
+  has_many :thumbnails, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :user
 
-  # tags
+  # tags settings
+  # acts_as_taggable
   acts_as_taggable_on :tags
+  attr_accessor :tags
 
   # nested form
   accepts_nested_attributes_for :thumbnails
+
+  # validation
+  validates :title, :copy, :concept, presence: true
+
+  def posted_date
+    created_at.strftime("%b %d")
+  end
+
+  def main_thumbnail
+    thumbnails.main.blank? ? "noimage-big.png" : thumbnails.main.first.thumbnail.to_s
+  end
+
+  def sub_thumbnails
+    thumbnails.sub.blank? ? ["noimage-big.png"] : thumbnails.sub.map(&:thumbnail)
+  end
+
+  def liked?(user)
+    likes.exists?(user_id: user.id)
+  end
+
+  def get_tags
+    Tag.where(name: tag_list)
+  end
+
+  def set_default_sub(i)
+    self.sub_thumbnails[i].present? ? sub_thumbnails[i].to_s : "noimage.png"
+  end
+
 end
