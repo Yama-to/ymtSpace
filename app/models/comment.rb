3efb73338665dcec5e53af252b284dc85a09a286
Yamato <wonka.bm@gmail.com>
2015-07-25 23:07:30 +0900

    resolve conflicts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,10 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :prototype
 
+  # validation
+  validates :text, presence: true
+
   def avatar_thumbnail
-    user.avatar_url(:thumbnail)
+    user.avatar_url(:thumb)
   end
 end
