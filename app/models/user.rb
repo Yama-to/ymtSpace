8611e51ebdbae3f3d9f87a23b49b702188436ec9
Yamato <wonka.bm@gmail.com>
2015-07-25 11:33:14 +0900

    modify devise default views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+  # association
+  has_many :prototypes, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # carrierwave
-  mount_uploader :avatar, AvatarUploader
+  mount_uploader :avatar, ImageUploader
 end
