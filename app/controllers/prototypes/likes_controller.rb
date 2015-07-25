e50a1c32452697e475dee9bc0b981c3208ef2620
Yamato <wonka.bm@gmail.com>
2015-07-25 23:09:54 +0900

    enables to like

--- app/controllers/prototypes/likes_controller.rb
+++ app/controllers/prototypes/likes_controller.rb
@@ -1,2 +1,18 @@
 class Prototypes::LikesController < ApplicationController
+  def create
+    Like.create(like_params)
+    redirect_to prototype_path(like_params[:prototype_id]) and return
+  end
+
+  def update
+    Like.find_by(like_params).destroy
+    redirect_to prototype_path(like_params[:prototype_id]) and return
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :prototype_id)
+  end
+
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :prototype, counter_cache: :likes_count
+
+  scope :find_or_init , ->(user_id, prototype_id){ where(user_id: user_id, prototype_id: prototype_id).first_or_initialize }
 end
