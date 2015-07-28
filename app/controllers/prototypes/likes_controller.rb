042ec59a3908b689fc3079a89275213071a505a5
Yamato <wonka.bm@gmail.com>
2015-07-28 17:26:21 +0900

    comment out return for redirect_to

--- app/controllers/prototypes/likes_controller.rb
+++ app/controllers/prototypes/likes_controller.rb
@@ -1,12 +1,12 @@
 class Prototypes::LikesController < ApplicationController
   def create
     Like.create(like_params)
-    redirect_to prototype_path(like_params[:prototype_id]) and return
+    redirect_to prototype_path(like_params[:prototype_id])# and return
   end
 
   def update
     Like.find_by(like_params).destroy
-    redirect_to prototype_path(like_params[:prototype_id]) and return
+    redirect_to prototype_path(like_params[:prototype_id])# and return
   end
 
   private
