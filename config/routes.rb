f03eac71c07cf5e4fac2e69f317584cf80fdbbc5
Yamato <wonka.bm@gmail.com>
2015-07-31 02:49:18 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,20 @@
 Rails.application.routes.draw do
-
   # for devise use
   devise_for :users
 
-  root 'prototypes#index'
-
-  resources :users,       only: [:show, :edit, :update]
-  resources :tags,        only: [:index, :show]
-  resources :prototypes,  except: [:destroy]
+  # standard routings
+  resources  :users,       only: [:show, :edit, :update]
+  resources  :tags,        only: [:index, :show]
+  resources  :prototypes,  except: [:destroy]
 
   # add prefix to controllers
   scope module: :prototypes do
-    resources :newest,    only: [:index], as: :newest_prototypes
-    resources :popular,   only: [:index], as: :popular_prototypes
-    resources :comments,  only: [:create]
-    resources :likes,     only: [:create, :update]
+    resources :newest,     only: [:index], as: :newest_prototypes
+    resources :popular,    only: [:index], as: :popular_prototypes
+    resources :comments,   only: [:create]
+    resources :likes,      only: [:create, :update]
   end
 
-  ## older-version
-  # add 2 actions other than resources
-  # resources :prototypes,  except: [:destroy] do
-  #   collection do
-  #     get 'newest'
-  #     get 'popular'
-  #   end
-  # end
-
+  # root_path must be placed on the bottom of routes
+  root 'prototypes#index'
 end
