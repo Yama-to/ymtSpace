a5c77d2831db6442b8fe0d9ab69028933a7d4626
Yamato <wonka.bm@gmail.com>
2015-08-05 15:33:34 +0900

    fix design

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,5 +4,4 @@ class Like < ActiveRecord::Base
 
   scope :find_or_init, ->(user_id, prototype_id){ where(user_id: user_id, prototype_id: prototype_id).first_or_initialize }
   scope :reset_like,   ->(like_data){ exists?(like_data) ? where(like_data).destroy_all : create(like_data) }
-
 end
