b941f15909f9975a8c28e4c35d6574185344b8be
Yamato <wonka.bm@gmail.com>
2015-08-05 10:28:58 +0900

    revise indent

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ActiveRecord::Base
   belongs_to :prototype, counter_cache: :likes_count
 
   scope :find_or_init, ->(user_id, prototype_id){ where(user_id: user_id, prototype_id: prototype_id).first_or_initialize }
-  scope :reset_like, ->(like_data){ exists?(like_data) ? where(like_data).destroy_all : create(like_data) }
+  scope :reset_like,   ->(like_data){ exists?(like_data) ? where(like_data).destroy_all : create(like_data) }
 
 end
