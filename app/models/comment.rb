5665d419cf23148c738ed221d71b402b30358932
Yamato <wonka.bm@gmail.com>
2015-08-09 17:51:05 +0900

    simplify by defining methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,15 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  belongs_to :prototype
+  belongs_to :prototype, counter_cache: :comments_count
 
   # validation
   validates :text, presence: true
 
   def avatar_thumbnail
     user.avatar_url(:thumb)
+  end
+
+  def user_name
+    user.name
   end
 end
